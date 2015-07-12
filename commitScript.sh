c3ff8e2b4b864c52dfc3d206c0949ea1d6f1e7f3
basimkhajwal <basimkhajwal@gmail.com>
2015-07-12 12:12:36 +0000

    Made some changes

--- commitScript.sh
+++ commitScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-cat "  Keepin' the streak alive  " >> streak.txt
+echo "  Keepin' the streak alive  " >> streak.txt
 git add *
 git commit -m "Kept the streak alive"
